b83e8f1cbc9856b16f7956c034d4fc13ff0bfcee
iKevinShah <kevin.mohan.shah@gmail.com>
2020-02-26 16:43:58 +0000

    Modified class and added README file

---
+++ README.md
@@ -0,0 +1,140 @@
+#Simple i18n
+ A simplistic PHP Internationalization and localization class.
+ 
+ The class helps achieve internationalization (i18n) via separate ini files for separate languages.
+### Installation
+
+Installation via composer
+````
+composer require ikevinshah/i18n
+````
+
+Installation without composer
+1. Download the class file (src/i18n.php).
+2. Include it in your code
+````php
+require_once '/path/to/i18n.php';
+````
+
+## Usage
+1. Install using either `composer` or `including` the class
+
+2. Set the variables as per your app, initialize and use `L::word` to display.
+````php
+$i18n = new ikevinshah\i18n($language,$ini_directory,$cache_directory,$fallback_language);
+$i18n->init();
+````
+**Note:** Ensure that `$language.ini` file exists in the `$ini_directory`. So example if  I want to use russian (ru) on the current page and the `$ini_directory` is `/var/www/lang/`, class will search for ini file at the location of `/var/www/lang/ru.ini`. If the language file (ru) is not found, class will fallback to use the fallback language. If the `$fallback_language.ini` file is also not found, it will throw an error and **exit**. 
+
+## Example 1 
+
+File: **/var/www/lang/en.ini**
+````ini
+CURRENT_LANG = 'en'
+HELLO = 'Hello'
+LOGIN = 'Login';
+SEARCH = 'Search'
+CATEGORIES = 'Categories';
+SETTINGS = 'Settings';
+PREVIOUS = 'Previous';
+NEXT = 'Next';
+X='Y';
+````
+
+PHP: 
+````php
+<?php
+
+require 'vendor/autoload.php';
+
+$i18n = new ikevinshah\i18n('en','/var/www/lang/','/tmp/php_cache/');
+$i18n->init();
+
+echo L::HELLO; // Hello
+
+echo L::CATEGORIES; //Categories
+````
+
+---
+
+## Example 2
+
+File: **/var/www/lang/fr.ini**
+````ini
+CURRENT_LANG = 'fr'
+HELLO = 'Bonjour'
+LOGIN = 'Connexion';
+SEARCH = 'Recherche'
+BACK = 'Retour à la page précédente';
+CATEGORIES = 'Catégories';
+SETTINGS = 'Paramètres';
+PREVIOUS = 'Précédent';
+NEXT = 'Suivant';
+X='Y';
+````
+
+PHP: 
+````php
+<?php
+
+require 'vendor/autoload.php';
+
+$i18n = new ikevinshah\i18n('fr','/var/www/lang/','/tmp/php_cache/','en');
+$i18n->init();
+
+echo L::HELLO; // Bonjour
+
+echo L::CATEGORIES; //Catégories
+````
+
+---
+
+## Example 3
+File: **/var/www/lang/fr.ini** -> Does not exist
+
+File: **/var/www/lang/en.ini**
+````ini
+CURRENT_LANG = 'en'
+HELLO = 'Hello'
+LOGIN = 'Login';
+SEARCH = 'Search'
+CATEGORIES = 'Categories';
+SETTINGS = 'Settings';
+PREVIOUS = 'Previous';
+NEXT = 'Next';
+X='Y';
+````
+
+PHP: 
+````php
+<?php
+
+require 'vendor/autoload.php';
+
+$i18n = new ikevinshah\i18n('fr','/var/www/lang/','/tmp/php_cache/','en');
+$i18n->init();
+
+echo L::HELLO; // Hello
+
+echo L::CATEGORIES; //Categories
+````
+
+## Example 4
+File: **/var/www/lang/fr.ini** -> Does not exist
+
+File: **/var/www/lang/en.ini** -> Does not exist
+
+PHP: 
+````php
+<?php
+
+require 'vendor/autoload.php';
+
+$i18n = new ikevinshah\i18n('fr','/var/www/lang/','/tmp/php_cache/','en');
+$i18n->init(); // Error: RuntimeException: Lang file for fr does not exist. in /path/to/src/i18n.php:78
+
+echo L::HELLO;
+
+echo L::CATEGORIES;
+````
+
--- src/i18n.php
+++ src/i18n.php
@@ -1,27 +1,67 @@
 <?php
 
-namespace iKevinShah;
+namespace ikevinshah;
 
+/**
+ * Class i18n
+ *
+ * @package ikevinshah
+ */
 class i18n
 {
-    
+    /**
+     * @var bool    To prevent repeated inits
+     */
     private $is_initialized = false;
     
+    /**
+     * @var string  The path of lang file
+     */
     private $lang_file = null;
     
+    /**
+     * @var string  The directory in which the compiled 'L' class will be stored
+     */
     private $cache_dir = null;
     
+    /**
+     * @var string  The lang in use.
+     */
     private $lang = null;
     
+    /**
+     * i18n constructor.
+     *
+     * @param string $required_lang     The lang to use.
+     * @param string $lang_dir          The directory in which the language ini files are stored.
+     * @param string $cache_dir         The directory in which to store the compiled file.
+     * @param string $fallback_lang     The lang to use in case the $required_lang is not found.
+     */
     final public function __construct (string $required_lang, string $lang_dir, string $cache_dir, string $fallback_lang = 'en')
     {
         try
         {
+            // Some PHP setups disable parse_ini_file for security purpose. Checking...
+            if(!\function_exists('parse_ini_file'))
+            {
+                //parse_ini_file function does not exist / is disabled.
+                throw new \RuntimeException('parse_ini_file function is disabled / unavailable.');
+            }
+            if(!\function_exists('file_exists'))
+            {
+                //file_exists function does not exist / is disabled.
+                throw new \RuntimeException('file_exists function is disabled / unavailable.');
+            }
+            if(!\function_exists('file_put_contents'))
+            {
+                //file_put_contents function does not exist / is disabled.
+                throw new \RuntimeException('file_put_contents function is disabled / unavailable.');
+            }
             //Does the cache directory exist?
             if (!$cache_dir || !\is_dir($cache_dir))
             {
                 //The cache directory is invalid / does not exist.
-                throw new \RuntimeException('Cache directory is invalid.');
+                throw new \RuntimeException('Cache directory is invalid / does not exist');
             }
             
             //Yes, the cache directory exists.
@@ -30,68 +70,83 @@ class i18n
             //Does the lang file exist?
             if (!\file_exists($lang_dir . $required_lang . '.ini'))
             {
-
                 //The lang file does not exist, does the fallback lang file exist?
                 if (!\file_exists($lang_dir . $fallback_lang . '.ini'))
                 {
                     // Both lang files are not found. Throw error
-                    throw new \RuntimeException('No lang file found.');
+                    throw new \RuntimeException('Lang file for '.$required_lang.' does not exist.');
                 }
-
                 //Fallback file exists, using that.
                 $this->lang = $fallback_lang;
                 $this->lang_file = $lang_dir . $fallback_lang . '.ini';
             }
             else
             {
-                //Lang file exists, use it.
+                //Lang file exists for $required_lang, use it.
                 $this->lang = $required_lang;
                 $this->lang_file = $lang_dir . $required_lang . '.ini';
             }
         }
         catch (\Exception $e)
         {
-            echo 'Error: ' . $e;
             //do something with the exception.
+            echo 'Error: ' . $e;
+            \error_log('i18n Error: '.$e);
+            exit;
         }
     }
     
+    /**
+     * Initialize and create the compiled file, if not already created.
+     * Also compiles the class again if the ini file for $required_lang is changed.
+     *
+     */
     final public function init()
     {
         //Run only if not already initialized
         if(!$this->is_initialized)
         {
-            //this is the file where compiled "L" class is stored.
+            //this is the file where compiled 'L' class is stored.
             $compiled_file = $this->cache_dir.'lang_'.$this->lang.'_compiled.php';
-
-            if(!\file_exists($compiled_file) || \filemtime($compiled_file) < \filemtime($this->lang_file))
+            
+            try
             {
-
-                $strings = \parse_ini_file($this->lang_file, false);
-                $compiled_class = '<?php'.PHP_EOL;
-                $compiled_class.= 'class L'.PHP_EOL;
-                $compiled_class.= '{'.PHP_EOL;
-                $compiled_class .= \PHP_EOL;
-                
-                foreach ($strings as $identifier => $value)
+                //if compiled file does not exist OR if ini file is modified after compile file was modified
+                if(!\file_exists($compiled_file) || \filemtime($compiled_file) < \filemtime($this->lang_file))
                 {
-                    $compiled_class .= '    const '.$identifier.' = \''.$value.'\';'.\PHP_EOL;
+                    $strings = \parse_ini_file($this->lang_file, false);
+                    $compiled_class = '<?php'.PHP_EOL;
+                    $compiled_class.= 'class L'.PHP_EOL;
+                    $compiled_class.= '{'.PHP_EOL;
+                    $compiled_class .= \PHP_EOL;
+                    
+                    foreach ($strings as $identifier => $value)
+                    {
+                        $compiled_class .= '    const '.$identifier.' = \''.$value.'\';'.\PHP_EOL;
+                    }
+                    $compiled_class.= '}'.PHP_EOL;
+                    
+                    if(empty($strings))
+                    {
+                        //Empty or corrupt ini file.
+                        throw new \RuntimeException('Empty or no content found in lang_file: '. $this->lang_file);
+                        
+                    }
+                    if(\file_put_contents($compiled_file, $compiled_class) === false)
+                    {
+                        throw new \RuntimeException('Error saving compiled language class.');
+                    }
                 }
-                $compiled_class .= \PHP_EOL;
-                $compiled_class.= '}'.PHP_EOL;
-                
-                if(empty($strings))
-                {
-                    error_log("Empty or no content found in lang_file: ". $this->lang_file);
-                    echo "Empty or no content found in lang_file";
-                }
-                if(\file_put_contents($compiled_file, $compiled_class) === false)
-                {
-                    error_log("Error saving compiled file.");
-                    echo "Error saving lang compiled file.";
-                }
+                require_once $compiled_file;
             }
-            require_once $compiled_file;
+            
+            catch (\Exception $e)
+            {
+                //do something with the exception.
+                echo 'Error: ' . $e;
+                \error_log('i18n Error: '.$e);
+                exit;
+            }
         }
     }
 }
